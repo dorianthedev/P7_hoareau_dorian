bcd569e6f3b3e30dc2b021b99243eb13bd77ccae
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-04 23:02:54 +0200

    creation commentaire good

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -1,45 +1,43 @@
 //importation connexion base de donnée mysql
 const mysqlconnection = require('../db/db.mysql');
-
 const fs = require('fs');
-
 const jwt = require('jsonwebtoken');
-
 const dotenv = require('dotenv');
 const { log } = require('console');
 const result = dotenv.config();
 
 exports.createComments = (req, res, next) => {
 
-      console.log("req.body coments");
-      console.log(req.body);
-
-        const id = req.params.id;
-        console.log("---> id comments");
-        console.log(id);
-
-        const userIdLocals = res.locals.userId;
-
-        // if (userIdLocals == id ) {
-        //   console.log("reussis");
-
-        //   mysqlconnection.query(
-        //     'INSERT INTO comments SET ?', commentsMess, (error, results, fields) => {
-        //         if (error) {
-        //             console.log(error);
-        //             res.json({error});
-        //         } else {
-        //             console.log("----> resultats");
-        //             console.log(results);
-        //             res.json({message : "Ton post est posté"})
-        //         }
-        //     }
-        // )
+        const idPost = req.params.id;
+        
+        
+        const commentsObject = JSON.parse(req.body.comments);
+        console.log("commentsObject");
+        console.log(commentsObject);
+                
+        commentsObject.comments_userId = res.locals.userId;
+        commentsObject.comments_postId = idPost;
 
 
-        // } else {
-        //   res.status(404).json({message: "erreur"})
-        // }
+        const comments = {
+          ...commentsObject
+          };
+
+          mysqlconnection.query(
+            'INSERT INTO comments SET ?', comments, (error, results, fields) => {
+                if (error) {
+                    console.log(error);
+                    res.json({error});
+                } else {
+                    console.log("----> resultats");
+                    console.log(results);
+                    res.json({message : "Ton post est posté"})
+                }
+            }
+          )
+
+
+        
 
     
 }
--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -78,6 +78,7 @@ exports.createPost = async (req, res, next) => {
                 }
             }
         )
+
     
    
 }
