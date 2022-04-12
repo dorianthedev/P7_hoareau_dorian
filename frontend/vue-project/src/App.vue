8db21aa13ea929d6202f7c0c018883e8f73971c0
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-12 15:33:11 +0200

    debut frontend

---
+++ frontend/vue-project/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import SignUp from './components/SignUp.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/icon-left-font-monochrome-black (1).png"/>
+
+  </header>
+
+  <main>
+    <SignUp />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  font-weight: normal;
+}
+
+.logo {
+  display: block;
+  width: 250px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+@media screen and (max-width: 450px) {
+  .logo {
+    width: 200px;
+  }
+}
+
+
+</style>
