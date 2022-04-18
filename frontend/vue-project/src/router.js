fa09daa13cab0639c716aa6d155776773157bdf2
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-18 21:16:38 +0200

    creation router login and html et css

--- frontend/vue-project/src/router.js
+++ frontend/vue-project/src/router.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from './views/Home.vue'
 import SignUp from './views/SignUp.vue'
+import Login from './views/Login.vue'
 
 const routes = [
     {
@@ -12,6 +13,11 @@ const routes = [
         name:'SignUp',
         component: SignUp,
         path:'/signup'
+    },
+    {
+        name:'Login',
+        component: Login,
+        path:'/login'
     }
 
 ]
