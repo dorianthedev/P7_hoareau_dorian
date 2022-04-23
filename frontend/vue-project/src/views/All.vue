6096d10d9ea4cbc49d5dcfd8ec36d1158b6644df
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-23 09:36:41 +0200

    correction redirection all to login

--- frontend/vue-project/src/views/All.vue
+++ frontend/vue-project/src/views/All.vue
@@ -10,25 +10,30 @@ export default {
             // if (!userLocalStorage) {
             //     this.$router.push({name:'Login'});
             // }
-
-            await fetch(`http://localhost:3000/api/post`, {
-                  
-                  headers: {
-                      "Content-type" : "application/json",
-                      'Authorization': `Bearer ${userLocalStorageToken.token}`
-                  },
-              })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Success:', data);
-            if (userLocalStorageToken.token !== data.token) {
+            
+            if (userLocalStorageToken) {
+                await fetch(`http://localhost:3000/api/post`, {
+                      
+                      headers: {
+                          "Content-type" : "application/json",
+                          'Authorization': `Bearer ${userLocalStorageToken.token}`
+                      },
+                })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                    if (userLocalStorageToken.token !== data.token) {
+                        this.$router.push({name:'Login'});
+        
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                }); 
+                
+            } else {
                 this.$router.push({name:'Login'});
-
             }
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          }); 
     }
 }
 </script>
