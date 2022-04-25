3a2ffde9bcf5a8e113e0d24c1017ac4c4c370344
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-25 12:54:10 +0200

    correction redirection all to login

--- frontend/vue-project/src/views/All.vue
+++ frontend/vue-project/src/views/All.vue
@@ -1,56 +1,42 @@
 <script>
 export default {
-    name:'All',
-    data()
-    {
-        return {
-            userId:'',
-            admin:'',
-            token:'',
-            allposts:[],
-            allusers:[]
-        }
-    },
-    methods: {
-        
-    },
-    async mounted() {
-            const userLocalStorageToken = JSON.parse(localStorage.getItem("login-user"));
-            
-            if (userLocalStorageToken) {
-                await fetch(`http://localhost:3000/api/post`, {
-                      
-                      headers: {
-                          "Content-type" : "application/json",
-                          'Authorization': `Bearer ${userLocalStorageToken.token}`
-                      },
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-                    if (userLocalStorageToken.token !== data.token || data.message == "Echec d'authentification") {
-                        this.$router.push({name:'Login'});
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                }); 
-                
-            } else {
-                this.$router.push({name:'Login'});
-            }
-    }
-}
+  name: "All",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {},
+  async mounted() {
+    const userLocalStorageToken = JSON.parse(
+      localStorage.getItem("login-user")
+    );
+
+    if (!userLocalStorageToken) return this.$router.push({ name: "Login" });
+
+    await fetch(`http://localhost:3000/api/post`, {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userLocalStorageToken.token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  },
+};
 </script>
 
 <template>
-    <section>
-        <div>
-            <h1>Bienvenue</h1>
-        </div>
-    </section>
+  <section>
+    <div>
+      <h1>Bienvenue</h1>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
