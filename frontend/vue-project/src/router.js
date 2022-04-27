e5071af60289d89d51394208c6dfd83731fe6659
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-27 14:30:05 +0200

    add route profil

--- frontend/vue-project/src/router.js
+++ frontend/vue-project/src/router.js
@@ -1,36 +1,41 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Home from './views/Home.vue'
-import SignUp from './views/SignUp.vue'
-import Login from './views/Login.vue'
-import All from './views/All.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import SignUp from "./views/SignUp.vue";
+import Login from "./views/Login.vue";
+import All from "./views/All.vue";
+import Profil from "./views/Profil.vue";
 
 const routes = [
-    {
-        name:'Home',
-        component: Home,
-        path:'/'
-    },
-    {
-        name:'SignUp',
-        component: SignUp,
-        path:'/signup'
-    },
-    {
-        name:'Login',
-        component: Login,
-        path:'/login'
-    },
-    {
-        name:'All',
-        component: All,
-        path:'/all'
-    }
-
-]
+  {
+    name: "Home",
+    component: Home,
+    path: "/",
+  },
+  {
+    name: "SignUp",
+    component: SignUp,
+    path: "/signup",
+  },
+  {
+    name: "Login",
+    component: Login,
+    path: "/login",
+  },
+  {
+    name: "All",
+    component: All,
+    path: "/all",
+  },
+  {
+    name: "Profil",
+    component: Profil,
+    path: "/profil",
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router;
