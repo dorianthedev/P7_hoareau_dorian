974e8acaff5060495c8f3ed6a31c196c9e931bed
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-04-29 21:05:32 +0200

    avancement affiche all users

--- frontend/vue-project/src/views/All.vue
+++ frontend/vue-project/src/views/All.vue
@@ -9,42 +9,37 @@ export default {
   },
   data() {
     return {
-      userId:'',
-      admin:'',
+      userId: "",
+      admin: "",
       posts: [],
     };
   },
   methods: {
     deleteAPost(postId) {
       const userLocalStorageToken = JSON.parse(
-      localStorage.getItem("login-user")
-    );
-      fetch(`http://localhost:3000/api/post/${postId}`, 
-            {
-                headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${userLocalStorageToken.token}`,
-                },
-                method: "DELETE",
-            })
-            .then(() => {
-                alert("La publication a bien été supprimée.");
-                window.location = "/all";
-            })
-    }
-    
+        localStorage.getItem("login-user")
+      );
+      fetch(`http://localhost:3000/api/post/${postId}`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userLocalStorageToken.token}`,
+        },
+        method: "DELETE",
+      }).then(() => {
+        alert("La publication a bien été supprimée.");
+        window.location = "/all";
+      });
+    },
   },
   async mounted() {
     const userLocalStorageToken = JSON.parse(
       localStorage.getItem("login-user")
     );
-  
-    
 
     if (!userLocalStorageToken) return this.$router.push({ name: "Login" });
 
-    this.userId = userLocalStorageToken.userId
-    this.admin = userLocalStorageToken.admin
+    this.userId = userLocalStorageToken.userId;
+    this.admin = userLocalStorageToken.admin;
 
     // afficher les posts sans les commentaires
     await fetch(`http://localhost:3000/api/post`, {
@@ -61,7 +56,6 @@ export default {
         }
         this.posts = data.results;
         console.log(this.posts);
-        
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -78,27 +72,33 @@ export default {
       <h3>Les posts des collègues</h3>
       <div class="main-block" v-for="post in posts" :key="post.id_post">
         <div class="block-post">
-          <div class="block-post__user">
-            <p>{{ post.firstName }} {{ post.lastName }}</p>
-          </div>
-          <div class="block-post__create">
-            <p>{{ post.post_date }}</p>
+          <div class="block-post__user-and-create">
+            <div class="block-post__create">
+              <p>Créer le {{ post.post_date }}</p>
+            </div>
+            <div class="block-post__user">
+              <p>De: {{ post.firstName }} {{ post.lastName }}</p>
+            </div>
+            
           </div>
           <div class="block-post__title">
-            <p>{{ post.post_title }}</p>
+            <p>Titre: {{ post.post_title }}</p>
           </div>
           <div class="block-post__message">
-            <p>{{ post.post_message }}</p>
+            <p>Message: {{ post.post_message }}</p>
           </div>
           <div class="block-post__image">
             <img
               class="image"
               v-if="post.post_image"
               :src="post.post_image"
-              alt="photo du profil de l'utilisatuer qui a écris la publication"
+              alt="photo"
             />
           </div>
-          <div class="bandeaubtn" v-if="post.id == userId || this.admin == 1 || this.admin == true">
+          <div
+            class="bandeaubtn"
+            v-if="post.id == userId || this.admin == 1 || this.admin == true"
+          >
             <button class="modify">
               <p>Modifier</p>
             </button>
@@ -122,11 +122,25 @@ export default {
 .main-block {
   display: flex;
   justify-content: center;
+  
 }
 
 .block-post {
   margin-bottom: 20px;
+  height: auto;
+  border-radius: 25px;
+  width:100%;
+  max-width: 600px;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.10);
 }
+
+
+
+.block-post__create {
+  font-size: 10px;
+}
+
 
 h3 {
   text-align: center;
@@ -134,9 +148,36 @@ h3 {
   font-weight: bold;
   text-transform: uppercase;
 }
-.image {
-  width: 300px;
-  height: 300px;
+.block-post__image {
+  width: inherit ;
+  height: 200px;
 }
 
+.image {
+  width: inherit;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 25px;
+}
+
+.bandeaubtn {
+  display: flex;
+  justify-content: center;
+  column-gap: 10px;
+  text-align: center;
+  margin-top: 5px;
+}
+
+.bandeaubtn button {
+  background-color: black;
+  color: black;
+  padding: 10px 10px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  opacity: 0.9;
+  color: white;
+  font-weight: bold;
+  border-radius: 50px;
+}
 </style>
