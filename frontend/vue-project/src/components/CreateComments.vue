989e2194c4389a714cefaccfddfde402202cf7cd
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-05-02 22:38:43 +0200

    debut create comments

---
+++ frontend/vue-project/src/components/CreateComments.vue
@@ -0,0 +1,37 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  name: "CreateComments",
+  data() {
+    return {
+      userId: "",
+      admin: "",
+      comments_messsage: "",
+    };
+  },
+  methods: {
+    async addComments(){
+
+
+      await fetch(`http://localhost:3000/api/post/:id/comments`, {
+          method: "POST",
+          body: JSON.stringify({
+            comments_messsage: this.comments_messsage,
+        
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+    }
+  },
+  mounted() {
+    const userLocalStorageToken = JSON.parse(
+      localStorage.getItem("login-user")
+    );
+  },
+};
+</script>
